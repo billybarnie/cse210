560fa237c8f52a6e2bf54d1de5cf7049b96e0f9d
Billy <haefguafe@gmail.com>
2023-04-20 22:49:53 -0500

    Assignment Prep4 program complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+
+        int sum = 0;
+
+        
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string num = Console.ReadLine();
+            number = int.Parse(num);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        }
+
+        foreach (int numb in numbers)
+        {
+            sum += numb;
+        }
+        
+        Console.WriteLine($"Your sum is: {sum}");
+
+        float aver = ((float)sum) / numbers.Count;
+        Console.WriteLine($"Your average altogether is: {aver}");
+
+        int largest = numbers[0];
+        
+        foreach (int numb in numbers)
+        {
+            if (numb > largest)
+            {
+                largest = numb;
+            }
+        }
+        
+        Console.WriteLine($"Your largest number is: {largest}");
     }
 }
