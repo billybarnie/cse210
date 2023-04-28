f5080a01b1d705b8d7f9049e832c68cfe140b542
Billy <haefguafe@gmail.com>
2023-04-27 21:04:35 -0500

    Assignment edits finished and finalized

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -46,7 +46,7 @@ class Journal
         {
             foreach (Entry entry in entries)
             {
-                newFile.WriteLine(entry.EntryAsCSV());
+                newFile.WriteLine(entry.EntryAsCSV().Trim());
             }
         }
     }
@@ -59,35 +59,71 @@ class Journal
         string yes = Console.ReadLine();
         string[] lines = System.IO.File.ReadAllLines(load);
 
+        // If user inputs "yes" executes the following code
         if (yes == "yes")
         {
+            // The entries List is Cleared of all data
             entries.Clear();
 
+            // Iterates through each block in the file that is being loaded as requested
             foreach (string block in lines)
-            {
+            {   
+                // The block is split by "|" and saved into the part variable
                 string[] part = block.Split("|");
-                string response = part[2];
-                string prompt = part[1];
-                string date = part[0];
+                // The third index is saved inside of the response variable
+                string response = part[2].Trim();
+                // The second index is saved inside of the prompt variable
+                string prompt = part[1].Trim();
+                // The first index is saved inside of the date variable
+                string date = part[0].Trim();
 
+                // A new Entry object is created and named "newentry"
                 Entry newentry = new Entry(date,prompt,response);
 
+                // The "newentry" object is then added to the entries List
                 entries.Add(newentry);
+
+                /* The data is being cleared to overwrite the data the user has previously entered
+                   being that they were asked if they would like to overwrite the current data that 
+                   they have entered if that have entered any data. From there the load file then 
+                   stores all the data from the file that they had requested to the "entries" list in 
+                   the format of {date}, {prompt}, {response}. In doing so they have then added the 
+                   file of journal entries requested to their journal.
+                */
 
             }
         }
+
+        // If user inputs "no" executes the following code
         else if (yes == "no")
-        {
+        {   
+            // Iterates through each block in the file that is being loaded as requested
             foreach (string block in lines)
             {
+                // The block is split by "|" and saved into the part variable
                 string[] part = block.Split("|");
-                string response = part[2];
-                string prompt = part[1];
-                string date = part[0];
+                // The third index is saved inside of the response variable
+                string response = part[2].Trim();
+                // The second index is saved inside of the prompt variable
+                string prompt = part[1].Trim();
+                // The first index is saved inside of the date variable
+                string date = part[0].Trim();
 
+                // A new Entry object is created and named "newentry"
                 Entry newentry = new Entry(date,prompt,response);
 
+                // The "newentry" object is then added to the entries List
                 entries.Add(newentry);
+
+                /* The data does not get cleared because the user decided not you overwrite any 
+                   current data that they had made as far as entries. The file that the user 
+                   requested is then loaded into the "entries" List in the same format that 
+                   it would be had had the overwritten as follows {date}, {prompt}, {response}.
+                   All entries they have entered plus the file requested contents will be added 
+                   to gether and it will be clearly seen when they use the display option(2). They 
+                   Can also add the same file or multiple other files to add all the responses 
+                   together with ease.
+                */
 
             }
         }
