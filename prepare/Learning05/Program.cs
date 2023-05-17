38f63e46aa4886faa3a3c723fc6da115cc6fc798
Billy Barnie <billybarnie@Billys-Air.lan>
2023-05-17 00:01:17 -0500

    Learning assignment finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square squ1 = new Square("Red", 5);
+        shapes.Add(squ1);
+
+        Rectangle rec1 = new Rectangle("blue", 7, 15);
+        shapes.Add(rec1);
+
+        Circle circ1 = new Circle("orange", 15);
+        shapes.Add(circ1);
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            Console.WriteLine($"Object with the color {shape.GetColor()} has an area of {area}");
+        }
     }
 }
